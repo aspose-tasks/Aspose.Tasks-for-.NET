7bfbe52c1094bc18cfdd8379ba22e6d5603b4407
aspose-email <email@aspose.com>
2018-06-07 23:06:48 +0500

    Updates for Aspose.Tasks for .NET 18.6

---
+++ Examples/CSharp/WorkingWithResourceAssignments/AddExtendedAttributesToRAWithLookUp.cs
@@ -0,0 +1,48 @@
+﻿using Aspose.Tasks.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Tasks.Examples.CSharp.WorkingWithResourceAssignments
+{
+    public class AddExtendedAttributesToRAWithLookUp
+    {
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
+
+            //ExStart: AddExtendedAttributesToRAWithLookUp
+            // Create new project
+            Project project = new Project(dataDir + "Blank2010.mpp");
+
+            var assignment = project.ResourceAssignments.First();
+
+            {
+                ExtendedAttributeDefinition resCostAttr = ExtendedAttributeDefinition.CreateLookupResourceDefinition(
+                    CustomFieldType.Cost,
+                    ExtendedAttributeResource.Cost5,
+                    "My lookup cost");
+
+                var value1 = new Value { NumberValue = 1500, Description = "Val 1", Id = 1, Val = "1500" };
+
+                resCostAttr.AddLookupValue(value1);
+
+                resCostAttr.AddLookupValue(new Value
+                {
+                    NumberValue = 2500,
+                    Description = "Val 2",
+                    Id = 2
+                });
+
+                project.ExtendedAttributes.Add(resCostAttr);
+
+                var value = resCostAttr.CreateExtendedAttribute(value1);
+                value.Value = "1500";
+                project.Save(dataDir + "AddExtendedAttributesToRAWithLookUp_out.mpp", SaveFileFormat.MPP);
+                //ExEnd: AddExtendedAttributesToRAWithLookUp
+            }
+        }
+    }
+}
---
+++ Examples/CSharp/WorkingWithResourceAssignments/AddExtendedAttributesToResourceAssignment.cs
@@ -0,0 +1,63 @@
+﻿using Aspose.Tasks.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Tasks.Examples.CSharp.WorkingWithResourceAssignments
+{
+    public class AddExtendedAttributesToResourceAssignment
+    {
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
+
+            //ExStart: AddExtendedAttributesToResourceAssignment
+            // Create new project
+            Project project = new Project(dataDir + "Blank2010.mpp");
+
+            // Add new task and resource
+            Task task1 = project.RootTask.Children.Add("Task");
+            Resource rsc1 = project.Resources.Add("Rsc");
+
+            // Assign the resource desired task
+            ResourceAssignment assn = project.ResourceAssignments.Add(task1, rsc1);
+
+            var assignment = project.ResourceAssignments.First();
+
+            // Custom attributes which is visible in "Resource Usage" view can be created with ExtendedAttributeDefinition.CreateResourceDefinition method.
+            {
+                ExtendedAttributeDefinition resCostAttr = ExtendedAttributeDefinition.CreateResourceDefinition(
+                    CustomFieldType.Cost,
+                    ExtendedAttributeResource.Cost5,
+                    "My cost");
+
+                project.ExtendedAttributes.Add(resCostAttr);
+
+                var value = resCostAttr.CreateExtendedAttribute();
+                value.Value = "1500";
+
+                assignment.ExtendedAttributes.Add(value);
+            }
+
+            // Custom attributes which is visible in "Task Usage" view can be created with ExtendedAttributeDefinition.CreateTaskDefinition method
+            {
+                ExtendedAttributeDefinition resCostAttr2 = ExtendedAttributeDefinition.CreateTaskDefinition(
+                    CustomFieldType.Cost,
+                    ExtendedAttributeTask.Cost5,
+                    "My cost for task");
+
+                project.ExtendedAttributes.Add(resCostAttr2);
+
+                var value = resCostAttr2.CreateExtendedAttribute();
+                value.Value = "2300";
+
+                assignment.ExtendedAttributes.Add(value);
+            }
+
+            project.Save(dataDir + "AddExtendedAttributesToResourceAssignment_out.mpp", SaveFileFormat.MPP);
+            //ExEnd: AddExtendedAttributesToResourceAssignment
+        }
+    }
+}
