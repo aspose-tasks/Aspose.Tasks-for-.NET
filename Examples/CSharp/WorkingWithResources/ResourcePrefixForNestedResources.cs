130d69bf3c23da2781a0372bc9c2b5686a3326c0
muzammil-khan <muzammil.khan@aspose.com>
2019-05-26 00:32:54 +0500

    Update ResourcePrefixForNestedResources.cs

--- Examples/CSharp/WorkingWithResources/ResourcePrefixForNestedResources.cs
+++ Examples/CSharp/WorkingWithResources/ResourcePrefixForNestedResources.cs
@@ -11,6 +11,8 @@ namespace Aspose.Tasks.Examples.CSharp.WorkingWithResources
     // ExStart:ResourcePrefixForNestedResources
     public class ResourcePrefixForNestedResources : ICssSavingCallback, IFontSavingCallback, IImageSavingCallback
     {
+        // The path to the documents directory.
+        public static string dataDir = RunExamples.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
         public static void Run()
         {
             // The path to the documents directory.
@@ -29,36 +31,36 @@ namespace Aspose.Tasks.Examples.CSharp.WorkingWithResources
 
         public void CssSaving(CssSavingArgs args)
         {
-            FileStream stream = new FileStream("css/" + args.FileName, FileMode.Create);
+            FileStream stream = new FileStream(dataDir + "css/" + args.FileName, FileMode.Create);
             args.Stream = stream;
             args.KeepStreamOpen = false;
-            args.Uri = "css/" + args.FileName;
+            args.Uri = dataDir + "css/" + args.FileName;
         }
 
         public void FontSaving(FontSavingArgs args)
         {
-            FileStream stream = new FileStream("fonts/" + args.FileName, FileMode.Create);
+            FileStream stream = new FileStream(dataDir + "fonts/" + args.FileName, FileMode.Create);
             args.Stream = stream;
             args.KeepStreamOpen = false;
-            args.Uri = "fonts/" + args.FileName;
+            args.Uri = dataDir + "fonts/" + args.FileName;
         }
 
         public void ImageSaving(ImageSavingArgs args)
         {
             if (args.FileName.EndsWith("png"))
             {
-                FileStream stream1 = new FileStream("resources/nestedResources/" + args.FileName, FileMode.Create);
+                FileStream stream1 = new FileStream(dataDir + "resources/nestedResources/" + args.FileName, FileMode.Create);
                 args.Stream = stream1;
                 args.KeepStreamOpen = false;
-                args.Uri = "resources/" + args.FileName;
-                args.NestedUri = "nestedResources/" + args.FileName;
+                args.Uri = dataDir + "resources/" + args.FileName;
+                args.NestedUri = dataDir + "nestedResources/" + args.FileName;
             }
             else
             {
-                FileStream stream2 = new FileStream("resources/" + args.FileName, FileMode.Create);
+                FileStream stream2 = new FileStream(dataDir + "resources/" + args.FileName, FileMode.Create);
                 args.Stream = stream2;
                 args.KeepStreamOpen = false;
-                args.Uri = "resources/" + args.FileName;
+                args.Uri = dataDir + "resources/" + args.FileName;
             }
         }
 
@@ -86,24 +88,24 @@ namespace Aspose.Tasks.Examples.CSharp.WorkingWithResources
             saveOptions.Pages.Clear();
             saveOptions.Pages.Add(pageNumber);
 
-            if (!Directory.Exists("fonts"))
+            if (!Directory.Exists(dataDir + "fonts"))
             {
-                Directory.CreateDirectory("fonts");
+                Directory.CreateDirectory(dataDir + "fonts");
             }
 
-            if (!Directory.Exists("resources"))
+            if (!Directory.Exists(dataDir + "resources"))
             {
-                Directory.CreateDirectory("resources");
+                Directory.CreateDirectory(dataDir + "resources");
             }
 
-            if (!Directory.Exists("nestedResources"))
+            if (!Directory.Exists(dataDir + "nestedResources"))
             {
-                Directory.CreateDirectory("resources/nestedResources");
+                Directory.CreateDirectory(dataDir + "resources/nestedResources");
             }
 
-            if (!Directory.Exists("css"))
+            if (!Directory.Exists(dataDir + "css"))
             {
-                Directory.CreateDirectory("css");
+                Directory.CreateDirectory(dataDir + "css");
             }
 
             return saveOptions;
