9d50293f6156616fd867a948dd9db603d6b29114
muzammil-khan <muzammil.khan@aspose.com>
2020-02-07 01:06:18 +0500

    Updates for Aspose.Tasks for .NET 20.1

---
+++ Examples/CSharp/WorkingWithProjects/CreatingReadingAndSaving/CreateEmptyProjectSaveMPP.cs
@@ -0,0 +1,26 @@
+﻿using Aspose.Tasks.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Tasks.Examples.CSharp.WorkingWithProjects.CreatingReadingAndSaving
+{
+    class CreateEmptyProjectSaveMPP
+    {
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
+
+            // ExStart:CreateEmptyProjectSaveMPP
+            // there is no more need to load MPP template to save it into MPP
+            // add tasks, resources, etc.
+            Project project = new Project();
+
+            // !The project will be saved into MPP by using internal MPP template.
+            project.Save(dataDir + "project.mpp", SaveFileFormat.MPP);
+            // ExEnd: CreateEmptyProjectSaveMPP
+        }
+    }
+}
