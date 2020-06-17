176da36c1755583bcc9fdca46c890528532890bf
Alexander Efremov <aedampir@gmail.com>
2020-06-17 19:37:29 +0700

    Updated examples.

--- Examples/CSharp/ExProject.cs
+++ Examples/CSharp/ExProject.cs
@@ -1263,32 +1263,6 @@
         }
 
         [Test]
-        public void ReadWriteProjectCustomProperties()
-        {
-#pragma warning disable 0618
-
-            // ExStart:ReadWriteProjectCustomProperties
-            // ExFor: Project.CustomProperties
-            // ExSummary: Shows how to read/write project's custom properties.
-            var project = new Project(DataDir + "Project2003.mpp");
-
-            project.CustomProperties.Add("My_Server", "http://server.com");
-            project.CustomProperties.Add("My_Boolean", true);
-            project.CustomProperties.Add("My_ProjectID", 1);
-            project.CustomProperties.Add("My_ProjectIDString", "1");
-            project.CustomProperties.Add("My_DeliverableID", 2);
-            project.CustomProperties.Add("My_ActivityID", 3);
-            project.CustomProperties.Add("My_RelatedFileID", 4);
-            project.CustomProperties.Add("My_GateDocumentID", 5);
-            project.CustomProperties.Add("My_TemplateLibraryID", 6);
-
-            Console.WriteLine(project.CustomProperties["My_Server"].ToString());
-
-            // ExEnd:ReadWriteProjectCustomProperties
-#pragma warning restore 0618
-        }
-
-        [Test]
         public void AddWBSCodes()
         {
             // ExStart:AddWBSCodes
