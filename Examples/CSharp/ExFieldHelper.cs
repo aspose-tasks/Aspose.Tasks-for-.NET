bc366b8f5fbb8b6b82cdb95f8b58470c3e84ecd2
Vasiliy Sinitsyn <vasiliy.sinitsyn@aspose.com>
2022-07-20 17:47:17 +0700

    Updated to Aspose.Tasks v.22.7.

---
+++ Examples/CSharp/ExFieldHelper.cs
@@ -0,0 +1,23 @@
+﻿using Aspose.Tasks.Util;
+
+namespace Aspose.Tasks.Examples.CSharp
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExFieldHelper : ApiExampleBase
+    {
+        [Test] 
+        public void WorkWithGetDefaultTaskFieldTitle()
+        {
+            // ExStart:WorkWithGetDefaultTaskFieldTitle
+            // ExFor: FieldHelper.GetDefaultTaskFieldTitle
+            // ExSummary: Shows how to get default field title for the specific task's field.
+
+            Console.WriteLine("Title for Tsk.ActualCost: " + FieldHelper.GetDefaultTaskFieldTitle(Tsk.ActualCost.KeyType));
+            Console.WriteLine("Title for Tsk.PercentWorkComplete: " + FieldHelper.GetDefaultTaskFieldTitle(Tsk.PercentWorkComplete.KeyType));
+            // ExEnd:WorkWithGetDefaultTaskFieldTitle
+        }
+    }
+}
