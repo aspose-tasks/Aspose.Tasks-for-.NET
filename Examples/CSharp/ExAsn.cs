e1fc262b01b01e8d80800d3df2f76bf8e4f1cb95
Vasiliy Sinitsyn <vasiliy.sinitsyn@aspose.com>
2023-05-19 01:03:53 +0700

    Updated to Aspose.Tasks for .NET v. 23.5.

--- Examples/CSharp/ExAsn.cs
+++ Examples/CSharp/ExAsn.cs
@@ -167,6 +167,42 @@
         }
 
         [Test]
+        public void SetResourceAssignmentCosts()
+        {
+            // ExStart:AutoCalculateAssignmentCosts
+            // ExFor: Prj.AutoCalculateAssignmentCosts
+            // ExFor: Project.AutoCalculateAssignmentCosts
+            // ExSummary: Shows how to turn of auto calculation of assignment's costs and to set assignment's costs explicitly.
+            var project = new Project();
+
+            var task = project.RootTask.Children.Add("New task");
+            task.Duration = project.GetDuration(TimeSpan.FromHours(15), TimeUnitType.Day);
+            var resource = project.Resources.Add("Resource");
+            resource.StandardRate = 10m;
+
+            var assignment = project.ResourceAssignments.Add(task, resource);
+
+            assignment.Work = project.GetDuration(TimeSpan.FromHours(12), TimeUnitType.Day);
+            assignment.ActualWork = project.GetDuration(TimeSpan.FromHours(3), TimeUnitType.Day);
+
+            Console.WriteLine("Now assignment's cost are auto calculated:");
+            Console.WriteLine("Actual Cost: {0}", assignment.ActualCost);
+            Console.WriteLine("Remaining Cost: {0}", assignment.RemainingCost);
+            Console.WriteLine("Cost: {0}", assignment.Cost);
+
+            project.AutoCalculateAssignmentCosts = false;
+            assignment.ActualCost = 123;
+            assignment.RemainingCost = 456;
+            assignment.Cost = 555;
+
+            Console.WriteLine("Now auto calculation of assignment's cost is turned off.");
+            Console.WriteLine("Actual Cost: {0}", assignment.ActualCost);
+            Console.WriteLine("Remaining Cost: {0}", assignment.RemainingCost);
+            Console.WriteLine("Cost: {0}", assignment.Cost);
+            // ExEnd:AutoCalculateAssignmentCosts
+        }
+
+        [Test]
         public void GetResourceAssignmentPercentWorkComplete()
         {
             // ExStart:GetResourceAssignmentPercentWorkComplete
